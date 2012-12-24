4379aade5b9fa44255fa13b48fcfa269c91a59a7
genya0407 <longinus.eva@gmail.com>
2012-12-24 11:19:13 +0900

    api取得までできた。が、ツイートしようとするとタイムスタンプが云々とエラーが出る。

---
+++ tweepy_test.py
@@ -0,0 +1,13 @@
+#coding: utf-8
+import tweepy
+import webbrowser
+
+consumer_key = 'HhFnDkO26i4Ct491Q5Zeg'
+consumer_secret = 'XGNL9HqyEvO3AQtX9dMeZSsdeRY7LwjOPYnz2TcFB0'
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+webbrowser.open(auth.get_authorization_url())
+pin = input('PIN: ').strip()
+auth.get_access_token(pin)
+auth.set_access_token(auth.access_token.key, auth.access_token.secret)
+api = tweepy.API(auth_handler=auth)
+api.update_status('てすと')
--- widgets.py
+++ widgets.py
@@ -1,6 +1,9 @@
 #coding: utf-8
 from PyQt4 import QtCore
 from PyQt4 import QtGui
+import tweepy
+import webbrowser
+
 
 class target(QtGui.QWidget):
 	def __init__(self, parent=None):
@@ -8,12 +11,12 @@ class target(QtGui.QWidget):
 		self.setUI()
 	
 	def setUI(self):
-		textbox = QtGui.QLineEdit()
-		button = QtGui.QPushButton('fav')
+		target_name = QtGui.QLineEdit()
+		trigger_button = QtGui.QPushButton('fav')
 		hbox = QtGui.QHBoxLayout()
 		
-		hbox.addWidget(textbox)
-		hbox.addWidget(button)
+		hbox.addWidget(target_name)
+		hbox.addWidget(trigger_button)
 		
 		self.setLayout(hbox)
 
@@ -40,18 +43,18 @@ class auth_widget(QtGui.QWidget):
 		self.setUI()
 	
 	def setUI(self):
-		add_user = QtGui.QPushButton('add')
-		verify_num = QtGui.QLineEdit()
-		launch_auth_window = QtGui.QPushButton('auth')
+		self.add_user = QtGui.QPushButton('add')
+		self.verify_num = QtGui.QLineEdit()
+		self.launch_auth_window = QtGui.QPushButton('auth')
 		
 		hbox = QtGui.QHBoxLayout()
-		hbox.addWidget(launch_auth_window)
-		hbox.addWidget(verify_num)
-		hbox.addWidget(add_user)
+		hbox.addWidget(self.launch_auth_window)
+		hbox.addWidget(self.verify_num)
+		hbox.addWidget(self.add_user)
 		
 		self.setLayout(hbox)
 
-class main_vbox(QtGui.QWidget):
+class main_widget(QtGui.QWidget):
 	def __init__(self, parent=None):
 		QtGui.QWidget.__init__(self, parent=parent)
 		self.setUI()
@@ -59,24 +62,55 @@ class main_vbox(QtGui.QWidget):
 	def setUI(self):
 		vbox = QtGui.QVBoxLayout()
 		
-		tg = target()
-		ul = user_list()
-		aw = auth_widget()
+		self.tg = target()
+		self.ul = user_list()
+		self.aw = auth_widget()
 		
-		vbox.addWidget(tg)
-		vbox.addWidget(ul)
-		vbox.addWidget(aw)
+		self.act = action()
+		
+			#結合
+		self.aw.launch_auth_window.clicked.connect(self.act.open_url)
+		self.aw.add_user.clicked.connect(self.pass_pin)
+		
+		vbox.addWidget(self.tg)
+		vbox.addWidget(self.ul)
+		vbox.addWidget(self.aw)
 		
 		self.setLayout(vbox)
+	
+	def pass_pin(self):
+		pin = self.aw.verify_num.text()
+#		print(pin)
+		self.act.create_api_object(pin)
+
+class action(object):
+	def __init__(self):
+		consumer_key = 'HhFnDkO26i4Ct491Q5Zeg'
+		consumer_secret = 'XGNL9HqyEvO3AQtX9dMeZSsdeRY7LwjOPYnz2TcFB0'
+		
+		self.auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+		self.api_list = []
+	
+	def open_url(self):
+		webbrowser.open(self.auth.get_authorization_url())
+	
+	def create_api_object(self, pin):
+		self.auth.get_access_token(pin)
+		self.auth.set_access_token(self.auth.access_token.key, self.auth.access_token.secret)
+		self.api = tweepy.API(auth_handler=self.auth)
+		self.api.update_status('てすと三回目')
+#		self.api_list.append(api)
+			#デバッグ用
+#		for i in self.api_list:
+#			i.update_status('てす')
 
 def main():
 	app = QtGui.QApplication([])
 	main_window = QtGui.QMainWindow()
-	
-	mw = main_vbox()
-	
+
+	mw = main_widget()
 	main_window.setCentralWidget(mw)
-	main_window.setWindowTitle('Weapons of mass favorite')
+	main_window.setWindowTitle('weapons_of_mass_favoritation')
 	main_window.show()
 	
 	app.exec_()
