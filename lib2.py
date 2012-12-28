70d4731dbdf5f9d8d61885af96384c9dd593d6db
genya0407 <longinus.eva@gmail.com>
2012-12-28 23:43:31 +0900

    一応動く。 コードの整理も一応した。 ・GUIとの関連付け ・fav_countを設定できるようにする ・すでにfavってるツイートに出くわした時に、更に深追いするような処理を組み込む
    複数ユーザー対応とかはその後でいいかな。

--- lib2.py
+++ lib2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import time, random, urllib, cgi, hmac, hashlib, base64, sys, webbrowser, requests
-from collections import OrderedDict
-from requests.auth import OAuth1
+import time, random, urllib, cgi, hmac, hashlib, base64, sys, webbrowser, json,requests
 
 class twitter(object):
 	def __init__(self,token=None):
@@ -16,7 +14,9 @@ class twitter(object):
 		self.request_token_url = 'http://twitter.com/oauth/request_token'
 		self.access_token_url = 'http://api.twitter.com/oauth/access_token'
 		self.update_url = 'https://api.twitter.com/1.1/statuses/update.json'
-		self.home_timeline_url = 'https://api.twitter.com/1/statuses/home_timeline.json'
+		self.home_timeline_url = 'https://api.twitter.com/1.1/statuses/home_timeline.json'
+		self.user_timeline_url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+		self.create_fav_url = 'https://api.twitter.com/1.1/favorites/create.json'
 
 		if token == None :
 			token = self.get_oauth_token()
@@ -29,9 +29,21 @@ class twitter(object):
 			'oauth_signature_method': 'HMAC-SHA1',
 			'oauth_timestamp': str(int(time.time())), 
 			'oauth_nonce': str(random.getrandbits(64)),
-			'oauth_version': '1.0'
-        }
-		
+			'oauth_version': '1.0'       }
+	
+	def params_str(self,params):
+		return '&'.join(['%s=%s' % (urllib.parse.quote(key, ''),urllib.parse.quote(params[key], '~')) for key in sorted(params)])
+	
+	def gen_message(self,method,url,params):
+		return '%s&%s&%s' % (method,urllib.parse.quote(url,''), urllib.parse.quote(self.params_str(params),''))
+	
+	def gen_sig_digest64(self,key,message):
+		hm = hmac.new(key.encode(),message.encode(),hashlib.sha1)
+		return base64.b64encode(hm.digest())
+	
+	def gen_header_str(self,params):
+		return 'OAuth ' + ','.join(['%s=%s' % (urllib.parse.quote(key,''),urllib.parse.quote(params[key],'~')) for key in sorted(params)])
+	
 	def get_oauth_token(self): 
 		params = {
 			'oauth_consumer_key': self.consumer_key,
@@ -94,28 +106,19 @@ class twitter(object):
 		return [token[b'oauth_token'][0].decode(), token[b'oauth_token_secret'][0].decode()] #[access_token, access_token_secret]
 
 	def update_status(self, text):
-
 		params = self.params
-
 		params['oauth_token'] = self.access_token
 		params['status'] = text
 		
-		params_str = '&'.join(['%s=%s' % (urllib.parse.quote(key, ''),urllib.parse.quote(params[key], '~')) for key in sorted(params)])
-		
-		message = '%s&%s&%s' % (self.method_2,urllib.parse.quote(self.update_url,''), urllib.parse.quote(params_str,''))
+		message = self.gen_message(self.method_2,self.update_url,params)
 		
 		key = '%s&%s' % (self.consumer_secret, self.access_token_secret)
-		
-		signature = hmac.new(key.encode(),message.encode(),hashlib.sha1)
-		digest_base64 = base64.b64encode(signature.digest())
-		
-		params['oauth_signature'] = digest_base64
+
+		params['oauth_signature'] = self.gen_sig_digest64(key,message)
 		
 		del params['status']
 		
-		header_params_str = ','.join(['%s=%s' % (urllib.parse.quote(key,''),urllib.parse.quote(params[key],'~'))
-																		for key in sorted(params)])
-		header_params_str = 'OAuth ' + header_params_str
+		header_params_str = self.gen_header_str(params)
 		
 		data = 'status='+urllib.parse.quote(text)
 		
@@ -126,35 +129,89 @@ class twitter(object):
 
 	def home_timeline(self):
 		params = self.params
+		params['oauth_token'] = self.access_token
 		
-		params_str = '&'.join(['%s=%s' % (urllib.parse.quote(key, ''),urllib.parse.quote(params[key], '~')) for key in sorted(params)])
-		
-		message = '%s&%s&%s' % (self.method,urllib.parse.quote(self.home_timeline_url,''), urllib.parse.quote(params_str,''))
-		
+		message = self.gen_message(self.method,self.home_timeline_url,params)
 		key = '%s&%s' % (self.consumer_secret, self.access_token_secret)
 		
-		signature = hmac.new(key.encode(),message.encode(),hashlib.sha1)
-		digest_base64 = base64.b64encode(signature.digest())
-		
-		params['oauth_signature'] = digest_base64
-		
-		header_params_str = ','.join(['%s=%s' % (urllib.parse.quote(key,''),urllib.parse.quote(params[key],'~'))
-																		for key in sorted(params)])
-		header_params_str = 'OAuth ' + header_params_str
+		params['oauth_signature'] = self.gen_sig_digest64(key,message)
 		
 		req = urllib.request.Request(self.home_timeline_url)
-		req.add_header('Authorization',header_params_str)
-		
-		res = urllib.request.urlopen(req)
-		print(res.read())
+		req.add_header('Authorization',self.gen_header_str(params))
 
+		res = urllib.request.urlopen(req)
+
+	def user_timeline(self,target):
+		params = self.params
+		params['oauth_token'] = self.access_token
+		params['screen_name'] = target
+		
+		message = self.gen_message(self.method,self.user_timeline_url,params)
+		key = '%s&%s' % (self.consumer_secret,self.access_token_secret)
+		
+		params['oauth_signature'] = self.gen_sig_digest64(key,message)
+		del params['screen_name']
+		
+		req_url = self.user_timeline_url + '?screen_name=' + urllib.parse.quote(target)
+		
+		req = urllib.request.Request(req_url)
+		req.add_header('Authorization',self.gen_header_str(params))
+		
+		return json.loads(urllib.request.urlopen(req).read().decode())
+		
+	def create_fav(self,tweet_id):
+		params = {
+			'oauth_consumer_key': self.consumer_key,
+			'oauth_signature_method': 'HMAC-SHA1',
+			'oauth_timestamp': str(int(time.time())), 
+			'oauth_nonce': str(random.getrandbits(64)),
+			'oauth_version': '1.0' 
+			}
+		params['oauth_token'] = self.access_token
+		params['id'] = tweet_id
+		
+		message = self.gen_message(self.method_2,self.create_fav_url,params)
+		key = '%s&%s' % (self.consumer_secret,self.access_token_secret)
+				
+		params['oauth_signature'] = self.gen_sig_digest64(key,message)
+		
+		print(message)
+		
+		del params['id']
+		
+		print(self.gen_header_str(params))
+		
+		data = 'id=' + urllib.parse.quote(tweet_id)
+		
+		req = urllib.request.Request(self.create_fav_url)
+		req.add_header('Authorization',self.gen_header_str(params))
+		req.add_data(data.encode())	
+		
+		try :
+			urllib.request.urlopen(req)
+		except urllib.error.HTTPError as e :
+			if e.code == '403' :
+				print('You have already faved it')
+			else :
+				print(e.code)
+				print(e.read())
+		
 def main():
 	
 	tw = twitter(['264147645-UUHUOZNxK0aPqSvXoW4mwG1zLrqmcTbCs1gMDnEA','cvcUfJtcWwYttQjhRYvzKOwWOurAuEuFWnQyLS39E'])
 	
 	#ついーと
-	#tw.update_status('おやー')
-	#tw.home_timeline()
+	#tw.update_status('関数の命名センスのかけらもない')
+	
+	twlist = tw.user_timeline('countboo')
+	idlist = []
+	for i in twlist :
+		idlist.append(i['id_str'])
+	
+	for i in idlist :
+		tw.create_fav(i)
+	
+	#print(twlist[0]['id_str'])
 
 if __name__ == '__main__':
 	main()
